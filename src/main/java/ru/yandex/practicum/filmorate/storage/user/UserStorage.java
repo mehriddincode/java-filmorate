74c7c07028289c9450713086a37c92b88d84481a
your-new-username <mehriddincode@gmail.com>
2026-04-15 16:55:01 +0500

    fix: add empty line separators between methods to pass Checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,14 +1,19 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 
 /**
  * Interface for User Storage.
  */
 public interface UserStorage {
+
     User create(User user);
+
     User update(User user);
+
     User getById(Long id);
+
     List<User> findAll();
 }
